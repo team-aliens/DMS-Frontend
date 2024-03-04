12b627089908720bb899993451f0f9ee1b840ad2
주원 <juwon2298@naver.com>
2024-03-04 23:00:06 +0900

    fix : path 수정

--- services/admin/src/apis/managers/index.ts
+++ services/admin/src/apis/managers/index.ts
@@ -1,9 +1,9 @@
 import { instance } from '..';
 import {
-  FindAccountIdResponse,
-  GetMyProfileResponse,
-  GetStudentDetailResponse,
-  SearchStudentListResponse,
+    FindAccountIdResponse,
+    GetMyProfileResponse,
+    GetStudentDetailResponse,
+    SearchStudentListResponse,
 } from './response';
 import { ResetPasswordRequest } from './request';
 import { PointType } from '../points';
@@ -17,97 +17,89 @@ const changeRouter = '/students';
 
 /** 아이디 찾기 */
 export const findId = async (schoolId: string, answer: string) => {
-  const { data } = await instance.get<Promise<FindAccountIdResponse>>(
-    `${router}/account-id/${schoolId}?answer=${answer}`
-  );
-  return data;
+    const { data } = await instance.get<Promise<FindAccountIdResponse>>(
+        `${router}/account-id/${schoolId}?answer=${answer}`,
+    );
+    return data;
 };
 
 /** 비밀번호 재설정 */
 export const resetPassword = async (body: ResetPasswordRequest) => {
-  await instance.patch(`${router}/password/initialization`, body);
+    await instance.patch(`${router}/password/initialization`, body);
 };
 
 /** 학생 검색 정렬 타입 */
 export type SortType = 'GCN' | 'NAME';
 export enum SortEnum {
-  GCN = '학번',
-  NAME = '이름',
+    GCN = '학번',
+    NAME = '이름',
 }
 export enum GenderEnum {
-  FEMALE = '여',
-  MALE = '남',
+    FEMALE = '여',
+    MALE = '남',
 }
 
 export const searchStudentList = async (
-  name: string,
-  sort: SortType,
-  filter_type: PointType,
-  min_point: number,
-  max_point: number,
-  tag_id: TagType[]
+    name: string,
+    sort: SortType,
+    filter_type: PointType,
+    min_point: number,
+    max_point: number,
+    tag_id: TagType[],
 ) => {
-  const tagIds = tag_id.map((res) => res.id).join('&tag_id=');
-  const { data } = await instance.get<SearchStudentListResponse>(
-    `${changeRouter}${router}?name=${name}&sort=${sort}&filter_type=${filter_type}&min_point=${min_point}&max_point=${max_point}${
-      tagIds && '&tag_id='
-    }${tagIds}`
-  );
-  return data;
+    const tagIds = tag_id.map((res) => res.id).join('&tag_id=');
+    const { data } = await instance.get<SearchStudentListResponse>(
+        `${changeRouter}${router}?name=${name}&sort=${sort}&filter_type=${filter_type}&min_point=${min_point}&max_point=${max_point}${
+            tagIds && '&tag_id='
+        }${tagIds}`,
+    );
+    return data;
 };
 
 export const getStudentDetail = async (student_id: string) => {
-  const { data } = await instance.get<Promise<GetStudentDetailResponse>>(
-    `${changeRouter}${router}/${student_id}`
-  );
-  return data;
+    const { data } = await instance.get<Promise<GetStudentDetailResponse>>(
+        `${changeRouter}/${student_id}`,
+    );
+    return data;
 };
 
 export const deleteStudent = async (student_id: string) => {
-  await instance.delete(`${changeRouter}${router}/${student_id}`);
+    await instance.delete(`${changeRouter}${router}/${student_id}`);
 };
 
 export const getMyProfile = async () => {
-  const { data } = await instance.get<GetMyProfileResponse>(
-    `${changeRouter}${router}/profile`
-  );
-  return data;
+    const { data } = await instance.get<GetMyProfileResponse>(`${changeRouter}${router}/profile`);
+    return data;
 };
 
 export const getStudentInfoExcel = () =>
-  useMutation(
-    () =>
-      instance.get(`${changeRouter}${router}/file`, {
-        responseType: 'blob',
-      }),
-    {
-      onSuccess: (res) => {
-        const blob = new Blob([res.data], {
-          type: res.headers['content-type'],
-        });
-        const fileName = res.headers['content-disposition'];
+    useMutation(
+        () =>
+            instance.get(`${changeRouter}${router}/file`, {
+                responseType: 'blob',
+            }),
+        {
+            onSuccess: (res) => {
+                const blob = new Blob([res.data], {
+                    type: res.headers['content-type'],
+                });
+                const fileName = res.headers['content-disposition'];
 
-        fileSaver.saveAs(blob, getFileNameFromContentDisposition(fileName));
-      },
-    }
-  );
+                fileSaver.saveAs(blob, getFileNameFromContentDisposition(fileName));
+            },
+        },
+    );
 
 export const uploadStudentInfoFile = async (file: FileList[0]) => {
-  const reqeustFile = new FormData();
-  reqeustFile.append('file', file);
-  const { data } = await instance.post(
-    `${changeRouter}${router}/file/gcn`,
-    reqeustFile
-  );
-  return data;
+    const reqeustFile = new FormData();
+    reqeustFile.append('file', file);
+    const { data } = await instance.post(`${changeRouter}${router}/file/gcn`, reqeustFile);
+    return data;
 };
 
 export const uploadRoomInfoFile = async (file: FileList[0]) => {
-  const reqeustFile = new FormData();
-  reqeustFile.append('file', file);
-  const { data } = await instance.post(
-    `${changeRouter}${router}/file/room`,
-    reqeustFile
-  );
-  return data;
+    const reqeustFile = new FormData();
+    reqeustFile.append('file', file);
+    const { data } = await instance.post(`${changeRouter}${router}/file/room`, reqeustFile);
+    return data;
 };

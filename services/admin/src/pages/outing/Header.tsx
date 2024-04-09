5d818d411c841f106eec588c5fd46b6c312f2490
juwon <juwon2298@naver.com>
2024-04-09 20:01:26 +0900

    fix: 외출 헤더 컴포넌트 수정

--- services/admin/src/pages/outing/Header.tsx
+++ services/admin/src/pages/outing/Header.tsx
@@ -1,22 +1,35 @@
 import { Arrow, Button, Text } from '@team-aliens/design-system';
 import styled from 'styled-components';
 import { Divider } from '@/components/main/Divider';
+import { useToast } from '@/hooks/useToast';
+import { ArrowLeft, ArrowRight } from '@/assets';
 
 interface HeaderProps {
   date: string;
+  onArrowClick: (increase: number) => void;
 }
 
-const Header = ({ date }: HeaderProps) => {
+const Header = ({ date, onArrowClick }: HeaderProps) => {
+  const { toastDispatch } = useToast();
+
+  const outingTileClick = () => {
+    toastDispatch({
+      actionType: 'APPEND_TOAST',
+      toastType: 'INFORMATION',
+      message: '개발 중인 기능입니다.',
+    });
+  };
+
   return (
     <_Container>
       <_DateBox>
-        <Arrow />
+        <img src={ArrowLeft} onClick={() => onArrowClick(-1)} />
         <Text size="headlineM" color="gray8">
           {date}
         </Text>
-        <Arrow direction="right" />
+        <img src={ArrowRight} onClick={() => onArrowClick(+1)} />
       </_DateBox>
-      <_ButtonWrapper>
+      <_ButtonWrapper onClick={outingTileClick}>
         <Button kind="outline" color="gray">
           엑셀 출력
         </Button>
@@ -33,7 +46,7 @@ const Header = ({ date }: HeaderProps) => {
 const _Container = styled.div`
   display: flex;
   justify-content: space-between;
-  margin: 20px;
+  margin: 50px 0px;
 `;
 
 const _ButtonWrapper = styled.div`

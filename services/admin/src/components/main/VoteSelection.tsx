4bb23f59f054ff7e5683e57eab307867939e44fd
seungWoo <ssounsowns@gmail.com>
2025-02-06 03:09:43 +0900

    feat: VoteSelect 컴포넌트 추가
    모달에서 선택지 컴포넌트

---
+++ services/admin/src/components/main/VoteSelection.tsx
@@ -0,0 +1,55 @@
+import { VoteEtcPlus, VoteModelStudentPlus } from '@/assets';
+import { font } from '@team-aliens/design-system/dist/styles/theme/font';
+import styled from 'styled-components';
+
+interface selectionType {
+  selectionType: string;
+}
+
+export const VoteSelection = ({ selectionType }: selectionType) => {
+  return (
+    <Wrapper>
+      {selectionType === 'etc' ? <VoteEtcPlus /> : <VoteModelStudentPlus />}
+
+      <Contents>
+        <span>{selectionType === 'etc' ? '기타' : '모범 학생'} 투표</span>
+        <div>
+          <p>{selectionType === 'etc' ? '기타' : '모범 학생'} 투표를</p> 만들
+          때에는 이곳을 클릭하세요!
+        </div>
+      </Contents>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  background-color: white;
+  border-radius: 24px;
+  box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 82px 88px;
+  gap: 28px;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0px 30px 40px rgba(0, 115, 255, 0.12),
+      0px 12px 20px rgba(0, 115, 255, 0.06);
+  }
+`;
+
+const Contents = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  align-items: center;
+  > span {
+    font: ${font.titleM};
+  }
+  > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;

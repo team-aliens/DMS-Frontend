d9c2f473cb88e7ce2ef51dbd83d43ef7aede082c
seungWoo <ssounsowns@gmail.com>
2025-02-05 22:19:11 +0900

    fix: 디자인 시스템 Modal 사용

--- services/admin/src/components/modals/CreateVoteModal.tsx
+++ services/admin/src/components/modals/CreateVoteModal.tsx
@@ -1,4 +1,10 @@
-import { Button, Input, Radio, TextArea } from '@team-aliens/design-system';
+import {
+  Button,
+  Input,
+  Modal,
+  Radio,
+  TextArea,
+} from '@team-aliens/design-system';
 import { color } from '@team-aliens/design-system/dist/styles/theme/color';
 import { font } from '@team-aliens/design-system/dist/styles/theme/font';
 import { useState } from 'react';
@@ -27,78 +33,76 @@ export const CreateVoteModal = ({ voteTopic }: VoteProps) => {
   };
 
   return (
-    <ModalBackground>
-      <Modal>
-        <Header>
-          투표 항목 생성
-          <Contents>
-            <Wrapper>
-              <InputBox>
-                <Input
-                  placeholder="모범학생 투표"
-                  label="투표 제목"
-                  name="투표 제목"
-                  onChange={onVoteTitleChange}
-                  value={voteTitle}
-                />
-
-                <Input
-                  placeholder="없음"
-                  label="투표 마감일"
-                  name="투표 마감일"
-                  onChange={() => {}}
-                  value={voteDate}
-                />
-
-                {voteTopic ? (
-                  <VoteTopicBox>
-                    투표 주제
-                    <RadioBox>
-                      {voteTopicRadios.map((data, index) => (
-                        <div
-                          key={index}
-                          onClick={() => setSelectedIndex(index)}
-                        >
-                          {data}
-                          <Radio
-                            className={selectedIndex === index ? 'checked' : ''}
-                          />
-                        </div>
-                      ))}
-                    </RadioBox>
-                  </VoteTopicBox>
-                ) : (
-                  <ButtonBox>
-                    <Button>투표 마감일 지정</Button>
-                  </ButtonBox>
-                )}
-              </InputBox>
-              {voteTopic && (
-                <ButtonBox>
-                  <Button>투표 마감일 지정</Button>
-                </ButtonBox>
-              )}
-            </Wrapper>
-            <TextAreaBox>
-              설명 추가
-              <TextArea
-                value={voteEx}
-                onChange={onVoteExChange}
-                width={680}
-                height={218}
-                placeholder="설명을 추가해주세요."
-              />
-            </TextAreaBox>
-          </Contents>
-        </Header>
+    <Modal
+      close={() => {}}
+      title="투표 항목 생성"
+      buttonList={[
         <ButtonDiv>
           <Button kind="outline">취소</Button>
           <Button disabled={!(voteTitle && voteDate && voteEx)}>
             {voteTopic ? '다음' : '확인'}
           </Button>
-        </ButtonDiv>
-      </Modal>
-    </ModalBackground>
+        </ButtonDiv>,
+      ]}
+      width="1150px"
+    >
+      <Contents>
+        <Wrapper>
+          <InputBox>
+            <Input
+              placeholder="모범학생 투표"
+              label="투표 제목"
+              name="투표 제목"
+              onChange={onVoteTitleChange}
+              value={voteTitle}
+            />
+
+            <Input
+              placeholder="없음"
+              label="투표 마감일"
+              name="투표 마감일"
+              onChange={() => {}}
+              value={voteDate}
+            />
+
+            {voteTopic ? (
+              <VoteTopicBox>
+                투표 주제
+                <RadioBox>
+                  {voteTopicRadios.map((data, index) => (
+                    <div key={index} onClick={() => setSelectedIndex(index)}>
+                      {data}
+                      <Radio
+                        className={selectedIndex === index ? 'checked' : ''}
+                      />
+                    </div>
+                  ))}
+                </RadioBox>
+              </VoteTopicBox>
+            ) : (
+              <ButtonBox>
+                <Button>투표 마감일 지정</Button>
+              </ButtonBox>
+            )}
+          </InputBox>
+          {voteTopic && (
+            <ButtonBox>
+              <Button>투표 마감일 지정</Button>
+            </ButtonBox>
+          )}
+        </Wrapper>
+        <TextAreaBox>
+          설명 추가
+          <TextArea
+            value={voteEx}
+            onChange={onVoteExChange}
+            width={680}
+            height={218}
+            placeholder="설명을 추가해주세요."
+          />
+        </TextAreaBox>
+      </Contents>
+    </Modal>
   );
 };
 
@@ -162,28 +166,4 @@ const InputBox = styled.div`
   display: flex;
   flex-direction: column;
   gap: 24px;
-`;
-
-const Modal = styled.div`
-  width: 1150px;
-  border-radius: 12px;
-  background-color: white;
-  font: ${font.headlineS};
-  padding: 34px 30px 34px 68px;
-  display: flex;
-  flex-direction: column;
-  gap: 264px;
-`;
-
-const ModalBackground = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0, 0, 0, 0.2);
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;

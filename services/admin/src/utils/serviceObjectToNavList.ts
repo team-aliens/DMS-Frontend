e004cb240f4750e38e54ed5993f6c1d56dec8504
ehdms42 <qwaszx080402@naver.com>
2025-02-17 00:05:52 +0900

    fix: 설문, 분실물 메뉴 추가

--- services/admin/src/utils/serviceObjectToNavList.ts
+++ services/admin/src/utils/serviceObjectToNavList.ts
@@ -5,6 +5,14 @@ export const serviceObjectToNavList = (features: IsUseAbleFeature) =>
   Object.entries(features)
     .map((i: [Features, boolean]) => serviceToKorean(i[0]))
     .concat({
+      service: '설문',
+      index: 3,
+    })
+    .concat({
+      service: '분실물',
+      index: 4,
+    })
+    .concat({
       service: '마이페이지',
       index: 5,
     })

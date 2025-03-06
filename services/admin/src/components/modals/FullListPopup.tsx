ce64fb89284958f1e6e9fee4334d737a456a91b1
seungWoo <ssounsowns@gmail.com>
2025-03-06 23:18:57 +0900

    feat: FullListPopup close 추가

--- services/admin/src/components/modals/FullListPopup.tsx
+++ services/admin/src/components/modals/FullListPopup.tsx
@@ -1,13 +1,21 @@
 import { Modal, Button } from '@team-aliens/design-system';
 
-export const FullListPopup = () => {
+interface PropsType {
+  onClose: () => void;
+}
+
+export const FullListPopup = ({ onClose }: PropsType) => {
   return (
     <Modal
-      close={() => {}}
+      close={onClose}
       title="항목이 가득찼습니다."
       buttonList={[
-        <Button kind="outline">취소</Button>,
-        <Button disabled={false}>{'확인'}</Button>,
+        <Button kind="outline" onClick={onClose}>
+          취소
+        </Button>,
+        <Button disabled={false} onClick={onClose}>
+          {'확인'}
+        </Button>,
       ]}
       width="560px"
     >

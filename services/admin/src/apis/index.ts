25c05b06a2922777b0f9d875403c7f5442a8f649
seungWoo <ssounsowns@gmail.com>
2025-03-07 19:26:51 +0900

    feat: 모범 학생 후보 삭제 모달

--- services/admin/src/apis/index.ts
+++ services/admin/src/apis/index.ts
@@ -1,4 +1,8 @@
-import axios, { AxiosError } from 'axios';
+import axios, {
+  AxiosError,
+  AxiosHeaders,
+  InternalAxiosRequestConfig,
+} from 'axios';
 import { reIssueToken } from '@/apis/auth';
 import { getCookie, setCookie } from '@/utils/cookies';
 
@@ -7,51 +11,98 @@ export const instance = axios.create({
   timeout: 10000,
 });
 
+let isRefreshing = false;
+let refreshSubscribers: ((token: string) => void)[] = [];
+
+const hasMessage = (data: any): data is { message: string } => {
+  return typeof data === 'object' && data !== null && 'message' in data;
+};
+
+const onRefreshed = (token: string) => {
+  refreshSubscribers.forEach((callback) => callback(token));
+  refreshSubscribers = [];
+};
+
 instance.interceptors.request.use(
   (config) => {
     const accessToken = getCookie('access_token');
-    const returnConfig = {
-      ...config,
-    };
     if (accessToken) {
-      //@ts-ignore
-      returnConfig.headers = {
+      config.headers = new AxiosHeaders({
+        ...config.headers,
         Authorization: `Bearer ${accessToken}`,
-      };
+      });
     }
-    return returnConfig;
+    return config;
   },
   (error: AxiosError) => Promise.reject(error),
 );
 
 instance.interceptors.response.use(
   (response) => response,
-  (error: AxiosError<AxiosError>) => {
-    if (axios.isAxiosError(error) && error.response) {
-      const { config } = error;
+  async (error: AxiosError) => {
+    const originalRequest = error.config as InternalAxiosRequestConfig & {
+      _retry?: boolean;
+    };
+
+    if (
+      axios.isAxiosError(error) &&
+      error.response?.data &&
+      hasMessage(error.response.data) &&
+      error.response.data.message === 'Expired Token'
+    ) {
       const refreshToken = getCookie('refresh_token');
-      if (error.response.data.message === 'Expired Token') {
-        if (refreshToken) {
-          reIssueToken(refreshToken).then((res) => {
-            const accessExpired = new Date(res.access_token_expired_at);
-            const refreshExpired = new Date(res.refresh_token_expired_at);
 
-            setCookie('access_token', res.access_token, {
-              expires: accessExpired,
+      if (!refreshToken) {
+        window.location.href = '/login';
+        return Promise.reject(error);
+      }
+
+      if (originalRequest._retry) {
+        return Promise.reject(error);
+      }
+      originalRequest._retry = true;
+
+      if (isRefreshing) {
+        return new Promise((resolve) => {
+          refreshSubscribers.push((token: string) => {
+            originalRequest.headers = new AxiosHeaders({
+              ...originalRequest.headers,
+              Authorization: `Bearer ${token}`,
             });
-
-            setCookie('refresh_token', res.refresh_token, {
-              expires: refreshExpired,
-            });
-
-            if (config.headers)
-              config.headers['Authorization'] = `Bearer ${res.access_token}`;
-            return axios(config);
+            resolve(instance(originalRequest));
           });
-        } else {
-          window.location.href = '/login';
-        }
-      } else return Promise.reject(error);
+        });
+      }
+
+      isRefreshing = true;
+
+      try {
+        const res = await reIssueToken(refreshToken);
+        const newAccessToken = res.access_token;
+
+        setCookie('access_token', newAccessToken, {
+          expires: new Date(res.access_token_expired_at),
+        });
+
+        setCookie('refresh_token', res.refresh_token, {
+          expires: new Date(res.refresh_token_expired_at),
+        });
+
+        isRefreshing = false;
+        onRefreshed(newAccessToken);
+
+        originalRequest.headers = new AxiosHeaders({
+          ...originalRequest.headers,
+          Authorization: `Bearer ${newAccessToken}`,
+        });
+        return instance(originalRequest);
+      } catch (err) {
+        isRefreshing = false;
+        window.location.href = '/login';
+        return Promise.reject(err);
+      }
     }
+
+    return Promise.reject(error);
   },
 );
---
+++ services/admin/src/components/modals/EditVoteStudent.tsx
@@ -0,0 +1,91 @@
+import { Magnifyingglass } from '@/assets/magnifyingglass';
+import { Button, Modal } from '@team-aliens/design-system';
+import { color } from '@team-aliens/design-system/dist/styles/theme/color';
+import { font } from '@team-aliens/design-system/dist/styles/theme/font';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+export function EditVoteStudent() {
+  const [name, setName] = useState<string>('');
+  const dummyData = [1101, 1102, 1103, 1104, 2213, 2201, 3201];
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setName(value);
+  };
+  return (
+    <Modal
+      buttonList={[<Button kind="outline">취소</Button>, <Button>확인</Button>]}
+      close={null}
+      width="1150px"
+    >
+      <_Wrapper>
+        <_Header>학생 수정</_Header>
+        <_Contents>
+          <_Input>
+            <Magnifyingglass />
+            <div></div>
+            <input type="text" onChange={onChange} value={name} />
+          </_Input>
+          <_Students>
+            {dummyData.map((data, index) => (
+              <_Item key={index}>
+                <span>{data}</span>
+              </_Item>
+            ))}
+          </_Students>
+        </_Contents>
+      </_Wrapper>
+    </Modal>
+  );
+}
+
+const _Item = styled.div`
+  padding: 21px 0 21px 36px;
+  border-radius: 4px;
+  box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.1);
+  width: 770px;
+  cursor: pointer;
+  > span {
+    font: ${font.bodyL};
+  }
+  &:hover {
+    background-color: ${color.primaryDarken1};
+    color: white;
+  }
+`;
+
+const _Students = styled.div``;
+
+const _Input = styled.div`
+  border-radius: 30px;
+  background-color: ${color.gray1};
+  box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.1);
+  width: 241px;
+  padding: 10px 0px 9px 14px;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  > div {
+    width: 1px;
+    height: 28px;
+    background-color: ${color.gray3};
+  }
+  > input {
+    font: ${font.bodyL};
+  }
+`;
+
+const _Contents = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const _Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+const _Header = styled.header`
+  font: ${font.headlineS};
+`;

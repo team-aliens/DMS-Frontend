3fd72edc952d53d094a91caed59b34147b95f534
seungW0o <ssounsowns@gmail.com>
2025-11-24 15:46:20 +0900

    feat: MyPage Skeleton 추가

---
+++ services/admin/src/components/common/Skeleton/index.ts
@@ -0,0 +1,5 @@
+export { Skeleton } from './Skeleton';
+export type { SkeletonProps, SkeletonVariant } from './Skeleton';
+export { StudentListSkeleton } from './StudentListSkeleton';
+export { DetailBoxSkeleton } from './DetailBoxSkeleton';
+export { MyPageSkeleton } from './MyPageSkeleton';

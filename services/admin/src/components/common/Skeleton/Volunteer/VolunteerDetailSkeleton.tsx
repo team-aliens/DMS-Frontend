3337dffafe5d7cc4869a696cb6afdfee244172fa
seungW0o <ssounsowns@gmail.com>
2025-12-01 00:41:18 +0900

    feat: 봉사활동 상세 페이지 Skeleton 추가
    봉사활동 상세 페이지 로딩 시 Skeleton UI를 보여주도록 구현
    - API 호출 중 로딩 상태를 시각적으로 표현하여 사용자 경험 개선
    - 데이터 로딩 완료 전까지 UI의 레이아웃 유지

---
+++ services/admin/src/components/common/Skeleton/Volunteer/VolunteerDetailSkeleton.tsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import { Skeleton } from '../Skeleton';
+
+export const VolunteerDetailSkeleton = () => {
+  return (
+    <_Wrapper>
+      <_ButtonContainer>
+        <Skeleton width="85px" height="47px" borderRadius="4px" />
+        <Skeleton width="85px" height="47px" borderRadius="4px" />
+      </_ButtonContainer>
+      <_StudentInfoContainer>
+        {new Array(5).fill(null).map((_, i) => (
+          <_StudentInfo>
+            <Skeleton width="10%" height="20px" />
+            <_InfoButtonContainer>
+              <Skeleton width="80px" height="50px" borderRadius="4px" />
+              <Skeleton width="80px" height="50px" />
+            </_InfoButtonContainer>
+          </_StudentInfo>
+        ))}
+      </_StudentInfoContainer>
+    </_Wrapper>
+  );
+};
+
+const _InfoButtonContainer = styled.div`
+  display: flex;
+  gap: 12px;
+`;
+
+const _Wrapper = styled.div`
+  margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+const _ButtonContainer = styled.div`
+  display: flex;
+  gap: 12px;
+  justify-content: flex-end;
+`;
+const _StudentInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const _StudentInfo = styled.div`
+  width: 990px;
+  height: 82px;
+  box-shadow: 0 1px 15px rgba(0, 0, 0, 0.1);
+  padding: 16px 24px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
--- services/admin/src/pages/volunteer/Detail.tsx
+++ services/admin/src/pages/volunteer/Detail.tsx
@@ -1,64 +1,51 @@
 import { WithNavigatorBar } from '@/components/WithNavigatorBar';
 import { StudentInfo } from '@/components/volunteer/StudentInfo';
 import styled from 'styled-components';
-import { VolunteerHeader } from './Header';
-import { useState, useEffect } from 'react';
-import { getApplicationVolunteerStudents } from '@/apis/volunteers';
 import { useParams } from 'react-router-dom';
-import { useToast } from '@/hooks/useToast';
 import { BreadCrumb } from '@team-aliens/design-system';
 import { pathToKorean } from '@/router';
+import {
+  useApplicationVolunteerStudents,
+  useApproveVolunteerApplication,
+  useRejectVolunteerApplication,
+} from '@/hooks/useVolunteerApi';
+import { VolunteerHeader } from './Header';
+import { VolunteerDetailSkeleton } from '@/components/common/Skeleton';
 
 export function VolunteerDetail() {
   const { id } = useParams<{ id: string }>();
-  const [applications, setApplications] = useState<any[]>([]);
-  const { toastDispatch } = useToast();
+  const { data, isLoading } = useApplicationVolunteerStudents(id ?? '');
 
-  useEffect(() => {
-    const fetchApplications = async () => {
-      try {
-        const response = await getApplicationVolunteerStudents(id);
-        setApplications(response?.applicants || []);
-      } catch (error) {
-        toastDispatch({
-          actionType: 'APPEND_TOAST',
-          toastType: 'ERROR',
-          message: '봉사 활동 신청된 학생 조회에 실패했습니다.',
-        });
-      }
-    };
-    fetchApplications();
-  }, [id]);
-
-  const handleApprove = (id: string) => {
-    setApplications((prevApplications) =>
-      prevApplications.filter((volunteer) => volunteer.id !== id),
-    );
-  };
-
-  const handleDelete = (id: string) => {
-    setApplications((prevApplications) =>
-      prevApplications.filter((volunteer) => volunteer.id !== id),
-    );
-  };
+  const { mutate: approveMutation } = useApproveVolunteerApplication();
+  const { mutate: rejectMutation } = useRejectVolunteerApplication();
 
   return (
     <WithNavigatorBar>
       <_Wrapper>
-        <BreadCrumb left={366} pathToKorean={pathToKorean}/>
-        <VolunteerHeader />
-        <_StudentInfoWrapper>
-          {applications.map((applicant) => (
-            <StudentInfo
-              key={applicant.id}
-              name={applicant.name}
-              gcd={applicant.gcd}
-              onApprove={handleApprove}
-              id={applicant.id}
-              onDelete={handleDelete}
-            />
-          ))}
-        </_StudentInfoWrapper>
+        <BreadCrumb left={366} pathToKorean={pathToKorean} />
+        {data?.applicants && isLoading ? (
+          <VolunteerDetailSkeleton />
+        ) : (
+          <>
+            <VolunteerHeader />
+            {data?.applicants.length === 0 ? (
+              <p>신청한 학생이 없습니다.</p>
+            ) : (
+              <_StudentInfoWrapper>
+                {data?.applicants.map((applicant) => (
+                  <StudentInfo
+                    key={applicant.id}
+                    name={applicant.name}
+                    gcd={applicant.gcd}
+                    onApprove={() => approveMutation(applicant.id)}
+                    id={applicant.id}
+                    onDelete={() => rejectMutation(applicant.id)}
+                  />
+                ))}
+              </_StudentInfoWrapper>
+            )}
+          </>
+        )}
       </_Wrapper>
     </WithNavigatorBar>
   );
